515501640cf4dcee9fba5d5bcea75cf9eb1992ac
Manuel Pecaoco III <92811949+vanitas10@users.noreply.github.com>
2025-05-08 11:18:31 +0800

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+    }
+}
